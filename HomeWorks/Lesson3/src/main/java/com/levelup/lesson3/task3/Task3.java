8e77cd48ab694b5baff40865d728ef7857e4d0d5
Daitin13 <Daitin13@gmail.com>
2016-08-01 13:09:35 +0300

    experiment

---
+++ HomeWorks/Lesson3/src/main/java/com/levelup/lesson3/task3/Task3.java
@@ -0,0 +1,4 @@
+package com.levelup.lesson3.task3;
+
+public class Task3 {
+}
---
+++ HomeWorks/Lesson4/src/main/java/com/levelup/lesson4/task3/Task3.java
@@ -0,0 +1,7 @@
+package com.levelup.lesson4.task3;
+
+/**
+ * Created by Daitin on 01.08.2016.
+ */
+public class Task3 {
+}
