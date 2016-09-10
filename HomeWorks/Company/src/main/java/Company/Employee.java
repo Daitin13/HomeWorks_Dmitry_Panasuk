3d0b4f2fa8fbf73fcee72370d969258e4afa7e4a
Daitin13 <Daitin13@gmail.com>
2016-09-10 15:02:30 +0300

    Hz

---
+++ HomeWorks/Company/src/main/java/Company/Employee.java
@@ -0,0 +1,56 @@
+package Company;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int salary;
+    private int IdEmployee;
+    private int IdCount = 0;
+
+    public Employee() {
+        this.name = "";
+        this.surname = "";
+        this.IdEmployee = ++IdCount;
+    }
+
+    public Employee(String name, String surname, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.IdEmployee = ++IdCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getIdEmloyee() {
+        return IdEmployee;
+    }
+
+    public void setIdEmployee(int idEmployee) {
+        IdEmployee = idEmployee;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+}
---
+++ HomeWorks/Company/src/main/java/Company/Manager.java
@@ -0,0 +1,5 @@
+package Company;
+
+public class Manager extends Employee{
+
+}
